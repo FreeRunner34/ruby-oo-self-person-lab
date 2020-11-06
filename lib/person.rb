4082468f406d091f632a27a30821405b8b8fee68
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-11-05 20:18:40 -0800

    commit tree ddbe3166d851d9badcfa98e655bade7f879352cb

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
-# your code goes here
+class Person
+    attr_reader :name
+
+    def initalize(name, happiness, bank_account)
+        @name = name 
+        @happiness = happiness
+        @bank_account = bank_account 
+    end 
+
+    def name 
+        @name 
+    end 
+
+    def happiness 
+        @happiness = 8
+    end 
+
+    def bank_account 
+        @bank_account = "$25"
+    end 
+end 
